ebc0fe46ad04f942487ee4e0194ecf3d7662a37f
Matthias Lienhard <lienhard@molgen.mpg.de>
2020-04-29 01:27:37 +0200

    update sahimi plots

--- isotools/stats.py
+++ isotools/stats.py
@@ -9,7 +9,7 @@ from pysam import  AlignmentFile
 from scipy.stats import binom,norm, chi2
 import statsmodels.stats.multitest as multi
 import isotools.splice_graph
-from isotools.transcriptome import log
+from isotools.transcriptome import log,junctions_from_cigar
 from tqdm import tqdm
 
 def overlap(pos1,pos2,width, height):
@@ -18,7 +18,11 @@ def overlap(pos1,pos2,width, height):
     return False
 
 #sashimi plots
-def sashimi_plot_bam(g, bam_fn,ax=None,text_width=.02, text_height=1, title=None,group=None, exon_color='blue', junction_color='green', min_junction_width=10, min_junction_cov=1,chrom_map=None):
+def sashimi_plot_bam(g, bam_fn,ax=None,text_width=.02, text_height=1, title=None,group=None, high_cov_th=.1,chrom_map=None,junctions_of_interest=None, exon_color='blue', 
+                low_cov_junctions={'color':'grey','lwd':1,'draw_label':False} , 
+                high_cov_junctions={'color':'green','lwd':1,'draw_label':True}, 
+                interest_junctions={'color':'purple','lwd':2,'draw_label':True}):
+    jparams=[low_cov_junctions,high_cov_junctions,interest_junctions]
     delta=np.zeros(g.end-g.start)
     chrom=g.chrom
     if chrom_map is not None:
@@ -29,7 +33,7 @@ def sashimi_plot_bam(g, bam_fn,ax=None,text_width=.02, text_height=1, title=None
     junctions={}
     with AlignmentFile(bam_fn, "rb") as align:
         for read in align.fetch(region=reg):        
-            blocks=read.get_blocks() #replace by exons?
+            blocks=junctions_from_cigar(read.cigartuples,read.reference_start) #alternative: read.get_blocks() 
             for i, block in enumerate(blocks):
                 s=max(g.start,min(g.end-1,block[0]))-g.start
                 e=max(g.start,min(g.end-1,block[1]))-g.start
@@ -37,14 +41,18 @@ def sashimi_plot_bam(g, bam_fn,ax=None,text_width=.02, text_height=1, title=None
                 delta[e]-=1            
                 if i>0:
                     jpos=(blocks[i-1][1],block[0])
-                    if jpos[1]-jpos[0]<min_junction_width:
+                    if jpos[1]-jpos[0]<1:
                         continue
                     junctions.setdefault(jpos,0)
                     junctions[jpos]+=1            
-    junctions={k:v for k,v in junctions.items() if not v<min_junction_cov}
-    cov=np.cumsum(delta)
+    cov=np.cumsum(delta)   
     if text_width<1:
         text_width=(g.end-g.start)*text_width
+    total_weight=max(cov)
+    if high_cov_th<1:
+        high_cov_th=high_cov_th*total_weight
+    junctions={k:v for k,v in junctions.items() if not v<1}
+
     if ax is None:
         fig,ax = plt.subplots(1)
         
@@ -68,12 +76,18 @@ def sashimi_plot_bam(g, bam_fn,ax=None,text_width=.02, text_height=1, title=None
              bow_height=(bow_height,bow_height+log10(y1/y2))
         else:
             bow_height=(bow_height,bow_height)
-
-        bow1=patches.Arc((center, log10(y1+.5)), width=width, height=bow_height[0]*2,theta1=90, theta2=180,linewidth=1,edgecolor=junction_color)
-        bow2=patches.Arc((center, log10(y2+.5)), width=width, height=bow_height[1]*2,theta1=0, theta2=90,linewidth=1,edgecolor=junction_color)
+        if junctions_of_interest is not None and (x1,x2) in junctions_of_interest:
+            priority=2
+        elif w< high_cov_th:
+            priority=0
+        else:
+            priority=1
+        bow1=patches.Arc((center, log10(y1+.5)), width=width, height=bow_height[0]*2,theta1=90, theta2=180,linewidth=jparams[priority]['lwd'],edgecolor=jparams[priority]['color'],zorder=priority)
+        bow2=patches.Arc((center, log10(y2+.5)), width=width, height=bow_height[1]*2,theta1=0, theta2=90,linewidth=jparams[priority]['lwd'],edgecolor=jparams[priority]['color'],zorder=priority)
         ax.add_patch(bow1)
         ax.add_patch(bow2)
-        txt=ax.text(center,log10(max(y1,y2)+.5)+min(bow_height)+text_height/3,w,horizontalalignment='center', verticalalignment='bottom',bbox=dict(boxstyle='round', facecolor='wheat',edgecolor=None,  alpha=0.5)).set_clip_on(True)
+        if jparams[priority]['draw_label']:
+            txt=ax.text(center,log10(max(y1,y2)+.5)+min(bow_height)+text_height/3,w,horizontalalignment='center', verticalalignment='bottom',bbox=dict(boxstyle='round', facecolor='wheat',edgecolor=None,  alpha=0.5)).set_clip_on(True)
         #bbox_list.append(txt.get_tightbbox(renderer = fig.canvas.renderer))
 
     #ax.set_yscale('log') 
@@ -93,106 +107,116 @@ def sashimi_plot(g, ax=None,text_width=.02, arc_type='coverage',text_height=1,ti
                 low_cov_junctions={'color':'grey','lwd':1,'draw_label':False} , 
                 high_cov_junctions={'color':'green','lwd':1,'draw_label':True}, 
                 interest_junctions={'color':'purple','lwd':2,'draw_label':True}):
-        jparams=[low_cov_junctions,high_cov_junctions,interest_junctions]
-        if 'splice_graph' not in g.data:
-            g.data['splice_graph']=isotools.splice_graph.SpliceGraph(g)
-        sg=g.data['splice_graph']
-        if title is None:
-            title=g.name
-        if group is None:
-            group=list(range(sg.weights.shape[0])) #all
-        
-        boxes=[(node[0], node[1], sg.weights[np.ix_(group,[i for i in set(node[2]).union(node[3])])].sum()) for node in sg]
-        if text_width<1:
-            text_width=(sg[-1][1]-sg[0][0])*text_width
-        total_weight=sg.weights[group,:].sum()
-        if high_cov_th<1:
-            high_cov_th=high_cov_th*total_weight
-        
-        idx=list(range(len(sg)))
-        arcs=[]
-        for i,(es,ee, pre, suc) in enumerate(sg._graph):
-            weights={}
-            for tr,next_i in suc.items():
-                weights.setdefault(next_i,0)
-                weights[next_i]+=sg.weights[np.ix_(group,[tr])].sum()
-            arcs_new=[(ee,boxes[i][2],sg[next_i][0],boxes[next_i][2],w) for next_i, w in weights.items() if sg[next_i][0]>ee and w>0]
-            if arcs_new:
-                arcs.extend(arcs_new)
-        if ax is None:
-            fig,ax = plt.subplots(1)
-            
-        for st, end, h in boxes:
-            if h>0:
-                rect = patches.Rectangle((st,0),(end-st),log10(h),linewidth=1,edgecolor=exon_color,facecolor=exon_color,zorder=5)
-                ax.add_patch(rect)
-        textpositions=[]
-        for x1,y1,x2,y2,w in arcs:        
-            center=(x1+x2)/2 
-            width=x2-x1
-            bow_height=text_height
-            while any(overlap((center,log10(max(y1,y2))+bow_height), tp,text_width,text_height) for tp in textpositions):
-                bow_height+=text_height
-            textpositions.append((center, log10(max(y1,y2))+bow_height))
-            if y1<y2: 
-                 bow_height=(log10(y2/y1)+bow_height,bow_height)
-            elif y1>y2:
-                 bow_height=(bow_height,bow_height+log10(y1/y2))
-            else:
-                bow_height=(bow_height,bow_height)
-            if junctions_of_interest is not None and (x1,x2) in junctions_of_interest:
-                priority=2
-            elif w< high_cov_th:
-                priority=0
-            else:
-                priority=1
-            bow1=patches.Arc((center, log10(y1)), width=width, height=bow_height[0]*2,theta1=90, theta2=180,linewidth=jparams[priority]['lwd'],edgecolor=jparams[priority]['color'],zorder=priority)
-            bow2=patches.Arc((center, log10(y2)), width=width, height=bow_height[1]*2,theta1=0, theta2=90,linewidth=jparams[priority]['lwd'],edgecolor=jparams[priority]['color'],zorder=priority)
-            ax.add_patch(bow1)
-            ax.add_patch(bow2)
-            if arc_type=='coverage':
-                lab=str(w)
-            else:                #fraction
-                lab=f'{w/total_weight:.1%}'
-                if arc_type=='both':
-                    lab=str(w)+' / '+lab
-            if jparams[priority]['draw_label']:
-                txt=ax.text(center,log10(max(y1,y2))+min(bow_height)+text_height/3,lab,
-                        horizontalalignment='center', verticalalignment='bottom',zorder=10+priority,
-                        bbox=dict(boxstyle='round', facecolor='wheat',edgecolor=None,  alpha=0.5)).set_clip_on(True)
-            #bbox_list.append(txt.get_tightbbox(renderer = fig.canvas.renderer))
-
-        #ax.set_yscale('log') 
-        ax.set_xlim(sg[0][0]-100, sg[-1][1]+100)
-        if textpositions:
-            ax.set_ylim(-text_height,max(tp[1] for tp in textpositions)+2*text_height)
-        else:
-            ax.set_ylim(-text_height,3) #todo: adjust y axis and ticklabels to coverage
-        ax.set(frame_on=False)
-        ax.set_yticks([0,1,2,3])
-        ax.set_yticklabels([1,10,100,1000])
-        
-        def mega(x, pos):
-            #'The two args are the value and tick position'
-            return f'{x*1e-6} MB'
-
-        mega_form = FuncFormatter(mega)
-        ax.xaxis.set_major_formatter(mega_form)
-
-        #ax.ticklabel_format(axis='x', style='sci',scilimits=(6,6))
-        #ax.set_xscale(1e-6, 'linear')
-        ax.set_title(title)
-        return(ax)
-
-def gene_plot(g, ax=None,title=None, draw_exon_numbers=True, color='blue'):
+    jparams=[low_cov_junctions,high_cov_junctions,interest_junctions]
+    if 'splice_graph' not in g.data:
+        g.data['splice_graph']=isotools.splice_graph.SpliceGraph(g)
+    sg=g.data['splice_graph']
+    if title is None:
+        title=g.name
+    if group is None:
+        group=list(range(sg.weights.shape[0])) #all
+    
+    boxes=[(node[0], node[1], sg.weights[np.ix_(group,[i for i in set(node[2]).union(node[3])])].sum()) for node in sg]
+    if text_width<1:
+        text_width=(sg[-1][1]-sg[0][0])*text_width
+    total_weight=sg.weights[group,:].sum()
+    if high_cov_th<1:
+        high_cov_th=high_cov_th*total_weight
+    
+    idx=list(range(len(sg)))
+    arcs=[]
+    for i,(es,ee, pre, suc) in enumerate(sg._graph):
+        weights={}
+        for tr,next_i in suc.items():
+            weights.setdefault(next_i,0)
+            weights[next_i]+=sg.weights[np.ix_(group,[tr])].sum()
+        arcs_new=[(ee,boxes[i][2],sg[next_i][0],boxes[next_i][2],w) for next_i, w in weights.items() if sg[next_i][0]>ee and w>0]
+        if arcs_new:
+            arcs.extend(arcs_new)
     if ax is None:
-        fig,ax = plt.subplots(1)    
-    for i,(trid, tr) in enumerate(g.transcripts.items()):
-        plt.plot(tr[exons][0][1], tr[exons][-1][0], [i+.25]*2)
+        fig,ax = plt.subplots(1)
+        
+    for st, end, h in boxes:
+        if h>0:
+            rect = patches.Rectangle((st,0),(end-st),log10(h),linewidth=1,edgecolor=exon_color,facecolor=exon_color,zorder=5)
+            ax.add_patch(rect)
+    textpositions=[]
+    for x1,y1,x2,y2,w in arcs:        
+        center=(x1+x2)/2 
+        width=x2-x1
+        bow_height=text_height
+        while any(overlap((center,log10(max(y1,y2))+bow_height), tp,text_width,text_height) for tp in textpositions):
+            bow_height+=text_height
+        textpositions.append((center, log10(max(y1,y2))+bow_height))
+        if y1<y2: 
+                bow_height=(log10(y2/y1)+bow_height,bow_height)
+        elif y1>y2:
+                bow_height=(bow_height,bow_height+log10(y1/y2))
+        else:
+            bow_height=(bow_height,bow_height)
+        if junctions_of_interest is not None and (x1,x2) in junctions_of_interest:
+            priority=2
+        elif w< high_cov_th:
+            priority=0
+        else:
+            priority=1
+        bow1=patches.Arc((center, log10(y1)), width=width, height=bow_height[0]*2,theta1=90, theta2=180,linewidth=jparams[priority]['lwd'],edgecolor=jparams[priority]['color'],zorder=priority)
+        bow2=patches.Arc((center, log10(y2)), width=width, height=bow_height[1]*2,theta1=0, theta2=90,linewidth=jparams[priority]['lwd'],edgecolor=jparams[priority]['color'],zorder=priority)
+        ax.add_patch(bow1)
+        ax.add_patch(bow2)
+        if arc_type=='coverage':
+            lab=str(w)
+        else:                #fraction
+            lab=f'{w/total_weight:.1%}'
+            if arc_type=='both':
+                lab=str(w)+' / '+lab
+        if jparams[priority]['draw_label']:
+            txt=ax.text(center,log10(max(y1,y2))+min(bow_height)+text_height/3,lab,
+                    horizontalalignment='center', verticalalignment='bottom',zorder=10+priority,
+                    bbox=dict(boxstyle='round', facecolor='wheat',edgecolor=None,  alpha=0.5)).set_clip_on(True)
+        #bbox_list.append(txt.get_tightbbox(renderer = fig.canvas.renderer))
+
+    #ax.set_yscale('log') 
+    ax.set_xlim(sg[0][0]-100, sg[-1][1]+100)
+    if textpositions:
+        ax.set_ylim(-text_height,max(tp[1] for tp in textpositions)+2*text_height)
+    else:
+        ax.set_ylim(-text_height,3) #todo: adjust y axis and ticklabels to coverage
+    ax.set(frame_on=False)
+    ax.set_yticks([0,1,2,3])
+    ax.set_yticklabels([1,10,100,1000])
+    
+    def mega(x, pos):
+        #'The two args are the value and tick position'
+        return f'{x*1e-6} MB'
+
+    mega_form = FuncFormatter(mega)
+    ax.xaxis.set_major_formatter(mega_form)
+
+    #ax.ticklabel_format(axis='x', style='sci',scilimits=(6,6))
+    #ax.set_xscale(1e-6, 'linear')
+    ax.set_title(title)
+    return(ax)
+
+def gene_track(g, ax=None,title=None, draw_exon_numbers=True, color='blue'):
+    if ax is None:
+        _,ax = plt.subplots(1)    
+    for i, tr in enumerate(g.transcripts.values()):
+        ax.plot((tr['exons'][0][0], tr['exons'][-1][1]), [i+.25]*2, color=color)
         #draw arrow to mark direction
-        for st, end in tr['exons']:
-            rect = patches.Rectangle((st,i),(end-st),i+.5,linewidth=1,edgecolor=color,facecolor=color)
+        for j,(st, end) in enumerate(tr['exons']):
+            rect = patches.Rectangle((st,i),(end-st),.5,linewidth=1,edgecolor=color,facecolor=color)
             ax.add_patch(rect)  
+            if draw_exon_numbers:
+                enr=j+1 if g.strand=='+' else len(tr['exons'])-j
+                ax.text((st+end)/2,i+.25,enr,ha='center', va='center',bbox=dict(boxstyle='round', facecolor='wheat',edgecolor=None,  alpha=0.5)).set_clip_on(True)    
+    ax.set_title(title)
+    ax.set(frame_on=False)
+    ax.set_yticks([i+.25 for i in range(g.n_transcripts)])
+    ax.set_yticklabels(g.transcripts)
+    ax.tick_params(left=False)
+    ax.set_ylim(-.5,g.n_transcripts+1)
+    ax.set_xlim(g.start-100, g.end+100)
     return ax
 
 

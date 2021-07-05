62ad1a8387d644381f02259f5030afd55bfe1b52
Matthias Lienhard <lienhard@molgen.mpg.de>
2021-07-05 15:08:12 +0200

    still fighting with dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-from src import __version__
+
+with open('../VERSION', 'r') as versionfile: #version string is the first line of this file
+    __version__=versionfile.read()
+
 # -- Project information -----------------------------------------------------
 
 project = 'isotools'

d5029166d8d089e224fe6b044751f422467e49ce
Matthias Lienhard <lienhard@molgen.mpg.de>
2021-11-16 18:18:34 +0100

    formated

--- tests/data_import_test.py
+++ tests/data_import_test.py
@@ -6,7 +6,7 @@ from isotools._utils import splice_identical
 @pytest.mark.dependency()
 def test_import_gff():
     isoseq = Transcriptome.from_reference('tests/data/example.gff.gz')
-    assert len(isoseq)==39, 'we expect 39 genes'
+    assert len(isoseq) == 39, 'we expect 39 genes'
     isoseq.save_reference('tests/data/example_ref_isotools.pkl')
     assert True
 
@@ -14,17 +14,17 @@ def test_import_gff():
 @pytest.mark.dependency(depends=['test_import_gff'])
 def test_import_bam():
     isoseq = Transcriptome.from_reference('tests/data/example_ref_isotools.pkl')
-    assert isoseq.n_transcripts==0, 'there should not be any transcripts'
+    assert isoseq.n_transcripts == 0, 'there should not be any transcripts'
     for sa in ('CTL', 'VPA'):
         isoseq.add_sample_from_bam(f'tests/data/example_1_{sa}.bam', sample_name=sa, group=sa, platform='SequelII')
-    assert isoseq.n_transcripts==185, 'we expect 185 transcripts'
+    assert isoseq.n_transcripts == 185, 'we expect 185 transcripts'
     isoseq.add_qc_metrics('tests/data/example.fa')
     isoseq.save('tests/data/example_1_isotools.pkl')
 
 
 @pytest.mark.dependency(depends=['test_import_bam'])
 def test_fsm():
-    isoseq = Transcriptome.load('tests/data/example_1_isotools.pkl')    
+    isoseq = Transcriptome.load('tests/data/example_1_isotools.pkl')
     for g, _, tr in isoseq.iter_transcripts(query='FSM'):
         assert tr['annotation'][0] == 0
         for ref_id in tr['annotation'][1]['FSM']:

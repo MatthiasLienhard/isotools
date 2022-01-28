6e260841e30301305318d3d45bac356c00637979
Carmine <ceraolo.carmine@gmail.com>
2022-01-28 14:20:20 +0100

    corrected a wrong import in transcriptome.py

--- src/isotools/transcriptome.py
+++ src/isotools/transcriptome.py
@@ -235,7 +235,7 @@ class Transcriptome:
     from ._transcriptome_filter import add_qc_metrics, add_filter, remove_filter, iter_genes, iter_transcripts, iter_ref_transcripts
 
     # statistic: differential splicing, alternative_splicing_events
-    from ._transcriptome_stats import altsplice_test, alternative_splicing_events, rarefaction, gene_coordination_test, coordination_test
+    from ._transcriptome_stats import altsplice_test, alternative_splicing_events, rarefaction, coordination_test
 
     # statistic: summary tables (can be used as input to plot_bar / plot_dist)
     from ._transcriptome_stats import altsplice_stats, filter_stats, transcript_length_hist, transcript_coverage_hist,\
